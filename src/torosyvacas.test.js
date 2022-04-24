45da079eb47c7a02d49d77917c06ab2beefd6017
BMB <bruno.marquez@ucb.edu.bo>
2022-04-24 15:48:16 -0400

    Muestra '' si no se adivina el codigo secreto

--- src/torosyvacas.test.js
+++ src/torosyvacas.test.js
@@ -1,15 +1,25 @@
 import sumar from "./sumador.js";
 
 describe("Toros y Vacas --> Ganar", () => {
-  it("Deberia responder 'Ganaste!' si se adivina el codigo secreto", () => {
-    const torosyvacas = new TorosYVacas("3");
-    expect(torosyvacas.adivinar("3")).toEqual("Ganaste!");
-  });
+    it("Deberia responder 'Ganaste!' si se adivina el codigo secreto", () => {
+        const torosyvacas = new TorosYVacas("3");
+        expect(torosyvacas.adivinar("3")).toEqual("Ganaste!");
+    });
+    it("Deberia responder '' si no se adivina el codigo secreto", () => {
+        const torosyvacas = new TorosYVacas("7");
+        expect(torosyvacas.adivinar("2")).toEqual("");
+    });
 });
 
 
 class TorosYVacas {
-  adivinar(intento) {
-    return "Ganaste!";
-  }
+    constructor(codigoSecreto) {
+        this.codigoSecreto = codigoSecreto;
+    }
+    adivinar(intento) {
+        if (intento === this.codigoSecreto) {
+            return "Ganaste!";
+        }
+        return "";
+    }
 }
