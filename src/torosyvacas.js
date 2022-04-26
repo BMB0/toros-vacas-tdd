998df0ad1dd8c455edcbb49b5cc54da12ab4acad
BMB <bruno.marquez@ucb.edu.bo>
2022-04-25 23:39:31 -0400

    Muestra dos vacas si el primer y el segundo caracter del intento se encuentran en el codigo secreto

--- src/torosyvacas.js
+++ src/torosyvacas.js
@@ -5,6 +5,8 @@ class TorosYVacas {
     adivinar(intento) {
         if (intento === this.codigoSecreto) {
             return "Ganaste!";
+        } else if (this.codigoSecreto.search(intento[0]) > -1 && this.codigoSecreto.search(intento[1]) > -1) {
+            return "**";
         } else if (this.codigoSecreto.search(intento[0]) > -1) {
             return "*";
         }
